224c6709c6379b7fc088a42fa10408c9dabb2b5f
pingu01 <99196159+pingu01@users.noreply.github.com>
2022-05-16 08:55:27 -0300

    correção de links

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 3/calculo.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 3/calculo.js
@@ -2,6 +2,7 @@ function arremesso(){
     var velocidade = Number(document.getElementById("name").value);
     var gravidade = 10
     var tempoDeQueda =velocidade/gravidade;
-    var alturaMax = (velocidade ** 2) / 2 * gravidade;
+    var alturaMax = (velocidade ** 2) / (2 * gravidade);
     document.getElementById("resultado").innerHTML = `Altura maxima: ${alturaMax} metros. <br> Tempo de queda ${tempoDeQueda} segundos.`
 }
+
