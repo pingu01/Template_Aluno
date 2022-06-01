bc78db92fd2dd1f090399adb2199492a407c17da
pingu01 <99196159+pingu01@users.noreply.github.com>
2022-06-01 20:07:30 -0300

    exercicios obrigatorios

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/ex_4/script.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/ex_4/script.js
@@ -1,19 +1,23 @@
 function calcular(){
+    //pega os valores dos inputs e transforma em variáveis
     var periodo = document.getElementById("periodo").value;
     var pessoas = document.getElementById("pessoas").value;
-    var valor = 0;
-    var desconto = 1;
-
-
+    var valor;
+    var desconto = 0;
+    // ve se o periodo é diurno ou noturno para determinar o valor e a quantidade de pessoas para determinar o desconto
     if(periodo == 1){
         valor = 200;
     } else if(periodo == 2){
         valor = 100;
     }
-
+    //ve a quantidade de pessoas para determinar o desconto
     if(pessoas > 50 && periodo == 1){
-        
-    } else if(periodo == 2){
-        
+        desconto = 0.4;
+    } else if(pessoas > 50 && periodo == 2){
+        desconto = 0.2;
     }
+
+    var valorFinal = valor - (valor * desconto);
+    document.getElementById("result").innerHTML = valorFinal;
+    console.log(valorFinal);
 }
